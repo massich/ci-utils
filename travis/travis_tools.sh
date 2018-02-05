23a82909acb8467d82171c54cef5433581b57c64
Joan Massich <sik@visor.udg.edu>
2018-02-05 14:19:41 +0100

    Add scripts

---
+++ travis/travis_tools.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script installs Travis helper functions and tools used in openmeeg repositories
+#
+# To use them, add the following lines in .travis.yml:
+#  - export DL_DIR=$HOME/downloads # Export the path where to store downloaded files
+#  - "curl -fsSkL TODO | sh"
+
+
+function _download() {
+    url="$1"; f="${2:-$(basename $url)}";
+    if [ ! -e $DL_DIR/$f ] ; then
+        mkdir -p $DL_DIR ;
+        echo "Downloading: ${DL_DIR}/$f" ;
+        travis_retry wget --no-verbose $url -O $DL_DIR/$f ;
+    else
+        echo "Reading from cache: ${DL_DIR}/$f" ;
+    fi
+}
+
+function download() { _download "$1" "" ; }
